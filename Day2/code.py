d722341cf23e4cc63832d47cd2c78bc9c2143e09
Krisje Jordaens <kris.jordaens@esc.be>
2021-12-02 22:05:40 +0100

    Day2

---
+++ Day2/code.py
@@ -0,0 +1,46 @@
+import math
+import os, sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from  AOCHelper import * 
+input = []
+def readinput():
+   global input
+   input = readinput_lines("Day2\input.txt")
+  
+def main():
+   readinput()
+   first_star()
+   second_star()        
+
+def first_star():
+   oper = {}
+   oper["forward"] = [1,0]
+   oper["down"] = [1,0]
+   oper["up"] = [-1,0]
+   
+   # make it unreadable :) :) 
+   for t in map(lambda x: x.split(),input):
+      oper[t[0]] = [oper[t[0]][0],oper[t[0]][1] + int(t[1])*oper[t[0]][0]]
+   
+   print("Result First Star")
+   print(oper["forward"][1] * (oper["down"][1]+oper["up"][1]))
+
+def second_star():
+   ho,ve,aim=0,0,0
+   for t in input:
+      
+      op,q=t.split()
+      q=int(q)
+    
+      if op == "forward":
+         ho+=q
+         ve+=aim*q
+      if op == "up":
+         aim-=q
+      if op == "down":
+         aim+=q
+  
+   print("Result Second Star")
+   print(ho*ve)
+if __name__ == '__main__':
+    main()
