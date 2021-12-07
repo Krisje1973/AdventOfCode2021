11030b8def67908929ec1333ed576abb871e49b4
Krisje Jordaens <kris.jordaens@esc.be>
2021-12-07 19:59:25 +0100

    Day7

--- Day7/code.py
+++ Day7/code.py
@@ -7,30 +7,30 @@ from  AOCHelper import *
 input = []
 def readinput():
    global input
-   global nums
+   global crabs
+   global dist
    input = readinput_lines_as_ints_with_separator("Day7\input.txt",",")
    input.sort()
+   crabs = dict(Counter(input))
+   dist = defaultdict(int)
 def main():
    readinput()
    first_star()
-   second_star()        
-
+   second_star()       
 def first_star():
-   crabs = dict(Counter(input))
-   dist = defaultdict(int)
+  
    for crab in range(len(input)+1):
       dist[crab] = sum([abs(crab - c) * crabs[c] for c in crabs if c != crab])
   
-   #l = [key  for (key, value) in dist.items() if value == min(dist.values())]
    print("Result First Star")
-   print( min(dist.values()))
+   print(min(dist.values()))
 
 def second_star():
-   crabs = dict(Counter(input))
-   dist = defaultdict(int)
+  
    for crab in range(len(input)+1):
       dist[crab] = sum([sum(range(abs(crab - c)+1)) * crabs[c] for c in crabs if c != crab])
+
    print("Result Second Star")
-   print( min(dist.values()))
+   print(min(dist.values()))
 if __name__ == '__main__':
     main()
