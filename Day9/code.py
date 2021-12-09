5546433d423409bd680389444c8c5be554bd77a1
Krisje Jordaens <kris.jordaens@esc.be>
2021-12-09 18:17:07 +0100

    Day9p2

--- Day9/code.py
+++ Day9/code.py
@@ -6,17 +6,19 @@ input = []
 def readinput():
    global input
    global nums
+   global maxx
+   global maxy 
    input = readinput_lines_skip_enters("Day9\input.txt")
    nums = list(map(int, input))
    nums = input
-def main():
-   readinput()
-   first_star()
-   second_star()        
-
-def first_star():
    maxx=len(str(nums[0]))-1
    maxy=len(nums)-1
+def main():
+   readinput()
+   #first_star()
+   second_star()        
+   #1315672 = to low
+def first_star():
    lows = []
    for x in range(maxx+1):
       for y in range(maxy+1):
@@ -28,7 +30,66 @@ def first_star():
    print(sum(lows))
    
 def second_star():
+   bassins = []  
+   for x in range(maxx+1):
+      for y in range(maxy+1):
+         ne = [int(str(nums[y])[x-1]) if x>0 else 9,int(str(nums[y])[x+1])if x<maxx else 9,int(str(nums[y-1])[x]) if y>0 else 9,int(str(nums[y+1])[x]) if y<maxy else 9]        
+         if int(str(nums[y])[x]) < min(ne):
+            bas = defaultdict(int)
+            basv = defaultdict(int)
+            bas[(x,y)] = 1
+           
+            for x,y in navigate(x,y,-1,0):  
+               bas[(x,y)] = 1
+            for x,y in navigate(x,y,+1,0):
+               bas[(x,y)] = 1
+
+            for x,y in bas.keys():
+               for k,v in navigate(x,y,0,-1):
+                  basv[(k,v)] = 1
+               for k,v in navigate(x,y,0,+1):
+                  basv[(k,v)] = 1
+
+            for x,y in basv:
+                bas[(x,y)] = 1
+
+            for x,y in basv:
+               for k,v in navigate(x,y,-1,0):
+                  bas[(k,v)] = 1
+               for k,v in navigate(x,y,+1,0):
+                  bas[(k,v)] = 1
+
+            bassins.append(sum(bas.values()))
+
+   bassins.sort()
+   tot = 1
+   for bas in bassins[len(bassins)-3:]:
+      tot *=  bas
    print("Result Second Star")
+   print(bassins)
+
+def navigate(x,y,x1,y1):
   
+   bas = defaultdict(int)
+   y+=y1
+   while y1 !=0 and y <= maxy and y>=0:
+      
+      if int(str(input[y])[x]) < 9:
+         bas[(x,y)] = 1
+      else:
+         break
+      y+=y1
+
+   x+=x1
+   while x1!=0 and x <= maxx and x>=0:
+      if int(str(input[y])[x]) < 9:
+         bas[(x,y)] = 1
+      else:
+         break
+      x+=x1
+
+   return bas
+       
+
 if __name__ == '__main__':
     main()
