61a32b8fe9c4aaf38fde2d6d49e9c7399a4f10a2
Krisje Jordaens <kris.jordaens@esc.be>
2021-12-10 13:46:52 +0100

    Day10p1

--- Day10/code.py
+++ Day10/code.py
@@ -5,21 +5,42 @@ from  AOCHelper import *
 input = []
 def readinput():
    global input
-   global nums
-   global boardsinput
-   input = readinput_lines_skip_enters("Day4\input.txt")
-   
-   nums = list(map(int, input[0].split(",")))
-   boardsinput = input[1:]
+   global tags
+   input = readinput_lines("Day10\input.txt")
+   tags = {}
+   tags['('] = ('(',1,3)
+   tags['['] = ('[',1,57)
+   tags['{'] = ('{',1,1197)
+   tags['<'] = ('<',1,25137)
+   tags[')'] = ('(',-1,1)
+   tags[']'] = ('[',-1,2)
+   tags['}'] = ('{',-1,3)
+   tags['>'] = ('<',-1,4)
+
    
 def main():
    readinput()
    first_star()
+   #187575 to low
+   #193275 
    second_star()        
 
 def first_star():
+   result = defaultdict(int)
+   for line in input:
+      open_sym = []
+      for tag in line:
+         if tags[tag][1] > 0:
+            open_sym.append(tag)
+         else:
+            if open_sym[-1] != tags[tag][0] or open_sym.count(tags[tag][0]) ==0:
+               result[tags[tag][0]] += tags[tags[tag][0]][2]
+               break
+            else:            
+               del open_sym[len(open_sym) - open_sym[::-1].index(tags[tag][0]) - 1]
+
    print("Result First Star")
- 
+   print(sum(result.values()))
 
 def second_star():
    print("Result Second Star")
