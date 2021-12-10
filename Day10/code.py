1f08f86a66b6bd31c6d7e73c71eef5b8bea21af9
Krisje Jordaens <kris.jordaens@esc.be>
2021-12-10 15:12:54 +0100

    Day10p2

--- Day10/code.py
+++ Day10/code.py
@@ -1,5 +1,6 @@
 import math
 import os, sys
+import statistics
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from  AOCHelper import * 
 input = []
@@ -8,10 +9,10 @@ def readinput():
    global tags
    input = readinput_lines("Day10\input.txt")
    tags = {}
-   tags['('] = ('(',1,3)
-   tags['['] = ('[',1,57)
-   tags['{'] = ('{',1,1197)
-   tags['<'] = ('<',1,25137)
+   tags['('] = ('(',1,3,1)
+   tags['['] = ('[',1,57,2)
+   tags['{'] = ('{',1,1197,3)
+   tags['<'] = ('<',1,25137,4)
    tags[')'] = ('(',-1,1)
    tags[']'] = ('[',-1,2)
    tags['}'] = ('{',-1,3)
@@ -20,7 +21,7 @@ def readinput():
    
 def main():
    readinput()
-   first_star()
+   #first_star()
    #187575 to low
    #193275 
    second_star()        
@@ -43,7 +44,29 @@ def first_star():
    print(sum(result.values()))
 
 def second_star():
+   result = []
+   for line in input:
+      open_sym = []
+      for tag in line:
+         if tags[tag][1] > 0:
+            open_sym.append(tag)
+         else:
+            if open_sym[-1] != tags[tag][0] or open_sym.count(tags[tag][0]) ==0:
+               open_sym.clear()
+               break
+            else:            
+               del open_sym[len(open_sym) - open_sym[::-1].index(tags[tag][0]) - 1]
+         
+     
+      if len(open_sym) > 0:
+         open_sym.reverse()
+         tot = 0
+         for o in open_sym:
+            tot = 5 * tot 
+            tot += tags[o][3]
+         result.append(tot)
+              
    print("Result Second Star")
-  
+   print(statistics.median(result))
 if __name__ == '__main__':
     main()
