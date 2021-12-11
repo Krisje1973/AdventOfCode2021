ddbbd3f41d0a20b62ceb98829aafe29d51dd7137
Krisje Jordaens <kris.jordaens@esc.be>
2021-12-11 14:04:14 +0100

    Day11 Cleanup

--- Day11/code.py
+++ Day11/code.py
@@ -9,6 +9,7 @@ def readinput():
    global octo
    global maxx
    global maxy
+   global result
 
    input = readinput_lines("Day11\input.txt")
    octo=[]
@@ -19,46 +20,55 @@ def readinput():
    maxy = len(octo) 
    
 def main():
+   global result
    readinput()
+   result =  list(map(int,count_flashes(100)))
    first_star()
    second_star()        
 
 def first_star():
+   print("Result First Star")
+   print(result[0])
+def second_star():
+  
+   print("Result Second Star")
+   print(result[1])
+
+def count_flashes(q):
    gh = GridHelper()
-   cnt = 0
+   cnt=0
+   cnt1=0
    while True:
       cnt+=1
     
       for r in octo:
          for c in range(maxy):      
             r[c] += 1 
-      #b = list([o for o in r for r in octo if o>9] )
+
       flashed = defaultdict(int)
       flash = list([(r,c) for c in range(maxx) for r in range(maxy) if octo[r][c]>9])
       
       while True:
          for r,c in flash:
             flashed[(r,c)] = 1
-            ne = gh.get_adjacent_pos_with_diag(c,r,maxx,maxy)
-            for x,y in ne:
+            for x,y in  gh.get_adjacent_pos_with_diag(c,r,maxx,maxy):
                octo[y][x] += 1
             octo[r][c] = 0
 
          flash = list([(r,c) for c in range(maxx) for r in range(maxy) if octo[r][c]>9])
          if len(flash)== 0 : break
 
-      #cnt += len(flashed)
+      cnt1 += len(flashed)
       for r,c in flashed:
          octo[r][c] = 0  
+
+      if cnt==q:
+         yield cnt1
       
-      if len(flashed) == 100: 
+      if len(flashed) == 100 : 
+         yield cnt
          break
-       
-   print("Result First Star")
-   print(cnt)
-def second_star():
-   print("Result Second Star")
-  
+   
 if __name__ == '__main__':
     main()
 
