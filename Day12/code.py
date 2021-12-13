8248cd9b6abc83357a30b23943e516383789a6c2
Krisje Jordaens <kris.jordaens@esc.be>
2021-12-13 08:06:30 +0100

    Day12p2

--- Day12/code.py
+++ Day12/code.py
@@ -3,26 +3,53 @@ import os, sys
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from  AOCHelper import * 
 input = []
+paths = defaultdict(list)
 def readinput():
    global input
-   global nums
-   global boardsinput
-   input = readinput_lines_skip_enters("Day4\input.txt")
+   global paths
+   input = readinput_lines("Day12\input.txt")
    
-   nums = list(map(int, input[0].split(",")))
-   boardsinput = input[1:]
-   
+   for inp in list([a for a in [line.split('-') for line in input]]):
+      paths[inp[0]].append(inp[1])
+      paths[inp[1]].append(inp[0])
+
 def main():
    readinput()
-   first_star()
+   #first_star()
    second_star()        
 
 def first_star():
    print("Result First Star")
- 
+   print(checkpath('start',{'start'}))
+
+def checkpath(cur,seen):
+   if cur == 'end': 
+      return 1
+   
+   total = 0
+   for path in [p  for p in paths[cur] if p not in seen]:
+      total += checkpath(path, seen | {path.lower()})
+  
+   return total
+
+def checkpath2(cur,seen,double):
+   if cur == "end":
+      return 1
+
+   total = 0
+   for path in paths[cur]:
+      if path.islower():
+         if path not in seen:
+            total += checkpath2(path, seen | {path}, double)
+         elif double and path != "start":
+            total += checkpath2(path, seen, False)
+      else:
+         total += checkpath2(path, seen, double)
+   return total 
 
 def second_star():
    print("Result Second Star")
+   print(checkpath2('start',{'start'},True))
   
 if __name__ == '__main__':
     main()
