37713f9ae758db1137facd48839dda1eb945bf05
Krisje Jordaens <kris.jordaens@esc.be>
2021-12-17 20:10:42 +0100

    Day17

--- Day17/code.py
+++ Day17/code.py
@@ -5,12 +5,9 @@ from  AOCHelper import *
 input = []
 def readinput():
    global input
-   global nums
-   global boardsinput
-   input = readinput_lines_skip_enters("Day4\input.txt")
-   
-   nums = list(map(int, input[0].split(",")))
-   boardsinput = input[1:]
+   global X,Y
+   rh = RegexHelper()
+   X, Y = map(rh.extract_numerics, open("Day17\input.txt", "r").read().split(","))
    
 def main():
    readinput()
@@ -18,8 +15,28 @@ def main():
    second_star()        
 
 def first_star():
+   best, total = 0
+   for y in range(-200, 200):
+      for x in range(0, 200):
+         pos = [0, 0]
+         v = [x, y]
+         maxy = 0
+         while pos[0] <= X[1] and Y[0] <= pos[1]:
+               maxy = max(maxy, pos[1])
+               if check_target(pos):
+                  best = max(best, maxy)
+                  total += 1
+                  break
+               pos[0] += v[0]
+               pos[1] += v[1]
+               if v[0] > 0:
+                  v[0] -= 1
+               v[1] -= 1
    print("Result First Star")
- 
+   print(best, total)
+
+def check_target(pos):
+    return X[0] <= pos[0] <= X[1] and Y[0] <= pos[1] <= Y[1]
 
 def second_star():
    print("Result Second Star")
