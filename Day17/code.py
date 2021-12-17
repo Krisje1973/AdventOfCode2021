d122fd45dc619516422012921585cde2429f94ec
Krisje Jordaens <kris.jordaens@esc.be>
2021-12-17 20:20:50 +0100

    Day17

--- Day17/code.py
+++ Day17/code.py
@@ -4,10 +4,9 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from  AOCHelper import * 
 input = []
 def readinput():
-   global input
-   global X,Y
+   global input,targetX,targetY
    rh = RegexHelper()
-   X, Y = map(rh.extract_numerics, open("Day17\input.txt", "r").read().split(","))
+   targetX, targetY = map(rh.extract_numerics, open("Day17\input.txt", "r").read().split(","))
    
 def main():
    readinput()
@@ -19,24 +18,24 @@ def first_star():
    for y in range(-200, 200):
       for x in range(0, 200):
          pos = [0, 0]
-         v = [x, y]
+         step = [x, y]
          maxy = 0
-         while pos[0] <= X[1] and Y[0] <= pos[1]:
+         while pos[0] <= targetX[1] and targetY[0] <= pos[1]:
                maxy = max(maxy, pos[1])
                if check_target(pos):
                   best = max(best, maxy)
                   total += 1
                   break
-               pos[0] += v[0]
-               pos[1] += v[1]
-               if v[0] > 0:
-                  v[0] -= 1
-               v[1] -= 1
+               pos[0] += step[0]
+               pos[1] += step[1]
+               if step[0] > 0:
+                  step[0] -= 1
+               step[1] -= 1
    print("Result First Star")
    print(best, total)
 
 def check_target(pos):
-    return X[0] <= pos[0] <= X[1] and Y[0] <= pos[1] <= Y[1]
+    return targetX[0] <= pos[0] <= targetX[1] and targetY[0] <= pos[1] <= targetY[1]
 
 def second_star():
    print("Result Second Star")
