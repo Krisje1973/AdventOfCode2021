5cae741c88248b5ab9b51078664602e4a44047b9
Krisje Jordaens <kris.jordaens@esc.be>
2021-12-25 20:21:21 +0100

    Day16 Cleanup

--- Day16/code.py
+++ Day16/code.py
@@ -125,43 +125,6 @@ def decode_operator(pack:Package):
          else:
             decode_operator(sub)
          pack.children.append(sub)
-
-def decode(bits):
-   global versions
-   global literals
-
-   while len(bits) > 5:
-      versions.append(int(bits[:3], 2))
-      bits=bits[3:]
-      type = int(bits[:3], 2)
-      bits=bits[3:]
-      if type == 4:
-         #prefix 1, then 4 digits, last = prefix 0
-         lit = ''
-         while True:
-            e = int(bits[:1], 2)
-            lit += bits[1:5]
-            bits=bits[5:]
-            if not e :
-               literals.append(lit)
-               break
-      else:
-         l = (15,11)
-       
-         ns = l[int(bits[:1],2)]
-        
-         sp = bits[1:ns+1]
-         sp = int(sp, 2)
-         bits=bits[ns+1:]
-         if ns == 15:
-            for i in range(sp//11):           
-               decode(bits[:11])
-            bits=bits[sp:]
-         else:
-            for i in range(sp):
-               b= bits[11:11*i+1]
-               bits=bits[11:]
-               decode(b)
   
 if __name__ == '__main__':
     main()
